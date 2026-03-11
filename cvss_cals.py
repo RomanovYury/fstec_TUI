b5b5cd30eed6edb97c22d8312cd8724d2c70e505
RomanovYury <romanovyury934@gmail.com>
2026-03-11 10:22:04 +0300

    почти работает

--- cvss_cals.py
+++ cvss_cals.py
@@ -1,19 +1,26 @@
-from cvss import CVSS3
-
+from cvss import CVSS3,CVSS4,CVSS2
+import numpy as np
 
 
 def calc_cvss(vector):
     
     """Принимает вектор, возвращает словарь score - оценка в цифрах, lvl - оценка в словах""" 
+    
 
-
-    c = CVSS3(vector)
+    if vector != "Нет данных" and str(vector)!= "nan":
+        if "CVSS:3.0" in vector or "CVSS:3.1" in vector:
+            c = CVSS3(vector)
+        elif "CVSS:4.0" in vector:
+            c = CVSS4(vector)
+        else:
+            c = CVSS2(vector)
+    else: 
+        return {"score" : -1}
     #print(c.scores()[0])
     #print(c.severities()[0])
     
     return {"score" : c.scores()[0],"lvl" : c.severities()[0],}
     
-
 
 
 

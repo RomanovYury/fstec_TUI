22f6319c94708003c874482991a8345905a936c8
RomanovYury <romanovyury934@gmail.com>
2026-03-19 10:04:09 +0300

    Update parser_word.py

--- parser_word.py
+++ parser_word.py
@@ -130,15 +130,15 @@ def generate(limit=None):
         doc.add_paragraph("2.1 Сведения о компонентах информационной системы определены в органе (организации) по результатам проведенной инвентаризации.")
         doc.add_paragraph("Исходя из данных инвентаризации определено:")
         doc.add_paragraph(f"Тип компонента информационной системы, подверженного уязвимости: {get_v(row, COL['type_comp'])}")
-        k_res = int( get_v(row, COL['k_k1']))  * int(get_v(row, COL['k_k2']))
+        k_res = float( get_v(row, COL['k_k1']))  * float(get_v(row, COL['k_k2']))
         add_centered_formula(doc, f"K=k ×K={get_v(row, COL['k_k1'])}×{get_v(row, COL['k_k2'])}={k_res}")
        
         doc.add_paragraph(f"Количество уязвимых компонентов информационной системы: {comp_text}")
-        l_res = int(get_v(row, COL['l_l1'])) * int(get_v(row, COL['l_l2']))
+        l_res = float(get_v(row, COL['l_l1'])) * float(get_v(row, COL['l_l2']))
         add_centered_formula(doc, f"L=l×L={get_v(row, COL['l_l1'])}×{get_v(row, COL['l_l2'])}={l_res}")
        
         doc.add_paragraph(f"Влияние на эффективность защиты периметра информационной системы: уязвимое программное, программно-аппаратное средство {net_text} из сети «Интернет».")
-        p_res = int(get_v(row, COL['p_p1'])) * int(get_v(row, COL['p_p2']))
+        p_res = float(get_v(row, COL['p_p1'])) * float(get_v(row, COL['p_p2']))
         add_centered_formula(doc, f"P=p×P={get_v(row, COL['p_p1'])}×{get_v(row, COL['p_p2'])}={p_res}")
 
         doc.add_paragraph("Таким образом, определяется показатель влияния уязвимости на функционирование информационной системы Iinfr.")

02381a1461e5f58aa79a275be93a411fe3fa4eef
RomanovYury <romanovyury934@gmail.com>
2026-03-19 18:13:30 +0300

    Add files via upload

--- cvss_cals.py
+++ cvss_cals.py
@@ -6,14 +6,21 @@ def calc_cvss(vector):
     
     """Принимает вектор, возвращает словарь score - оценка в цифрах, lvl - оценка в словах""" 
     
-
+    print(vector)
     if vector != "Нет данных" and str(vector)!= "nan":
         if "CVSS:3.0" in vector or "CVSS:3.1" in vector:
             c = CVSS3(vector)
         elif "CVSS:4.0" in vector:
             c = CVSS4(vector)
-        else:
+        elif "CVSS:2.0" in vector: 
             c = CVSS2(vector)
+        else:
+            if "Au:" in vector: 
+                c = CVSS2(vector)
+            else:
+                if "CVSS 3.0/" in vector:
+                    vector = vector.replace("CVSS 3.0/", "CVSS:3.0/")
+                c = CVSS3("CVSS:3.1/"+vector)
     else: 
         return {"score" : -1}
     #print(c.scores()[0])
